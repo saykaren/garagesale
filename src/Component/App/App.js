31b580849ec57982dd3d6f122e672da37e5ce092
Karen Garcia <saykaren@gmail.com>
2022-05-16 19:32:53 -0600

    First commit of project

---
+++ src/Component/App/App.js
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import "../Styling/App.css";
+import Footer from './Footer';
+
+const App = () => {
+  return ( 
+    <>
+    <div>I am here</div>
+        <Footer/>
+    </>
+
+   );
+}
+ 
+export default App;
+
